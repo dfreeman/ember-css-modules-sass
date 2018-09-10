76bd5143b5834686532e6d2c573e603066987801
Dan Freeman <dfreeman@salsify.com>
2018-09-10 16:36:26 -0400

    Fix "missing package directory" noise

--- tests/acceptance/verify-styles-test.js
+++ tests/acceptance/verify-styles-test.js
@@ -1,18 +1,17 @@
-import $ from 'jquery';
 import { module, test } from 'qunit';
 import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
-module('Acceptance | verify styles v2', function(hooks) {
+module('Acceptance | verify styles', function(hooks) {
   setupApplicationTest(hooks);
 
   test('app and addon styles are processed', async function(assert) {
     await visit('/');
 
-    let $appComponent = $('[data-app-component]');
-    let $addonComponent = $('[data-addon-component]');
+    let appComponent = this.element.querySelector('[data-app-component]');
+    let addonComponent = this.element.querySelector('[data-addon-component]');
 
-    assert.equal(window.getComputedStyle($appComponent[0]).fontFamily, 'app');
-    assert.equal(window.getComputedStyle($addonComponent[0]).fontFamily, 'addon');
+    assert.equal(window.getComputedStyle(appComponent).fontFamily, 'app');
+    assert.equal(window.getComputedStyle(addonComponent).fontFamily, 'addon');
   });
 });
